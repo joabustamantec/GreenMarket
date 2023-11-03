260eb54d4df033552c431390c419b605ebb264b9
alexitoso <82743599+alexitoso@users.noreply.github.com>
2023-11-03 02:38:57 -0300

    areglo bostrap

--- bdgreen/bdgreen/urls.py
+++ bdgreen/bdgreen/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from login import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.HomeView.as_view(), name="home"),
-    path("", views.iniciar),
 ]
